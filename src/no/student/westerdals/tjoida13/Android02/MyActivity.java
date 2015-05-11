71cb3718b593eafed249d589ec8ce4576aad6feb
IdarV <tjoida13@nith.no>
2015-05-11 15:28:43 +0200

    Basic storage and getting/set views

--- src/no/student/westerdals/tjoida13/Android02/MyActivity.java
+++ src/no/student/westerdals/tjoida13/Android02/MyActivity.java
@@ -2,8 +2,15 @@ package no.student.westerdals.tjoida13.Android02;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MyActivity extends Activity {
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +18,12 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        String[] myList = getResources().getStringArray(R.array.things);
+        ArrayList<String> myArrayList= new ArrayList<String>(Arrays.asList(myList));
+        Log.d("oncreate", "array length: " + myList.length);
+        TextView txtView1 = (TextView) findViewById(R.id.textView4);
+        Log.d("oncreate", "setting 4 to " + myList[6] + "88");
+        txtView1.setText(myList[6] + "88");
     }
 }
